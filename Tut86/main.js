5bb2750bd67ff7f175d9160ac6d6f5332583891b
LogicalLuminary <amritamrendranmol@gmail.com>
2025-07-20 23:58:01 +0530

    Day 21

---
+++ Tut86/main.js
@@ -0,0 +1,11 @@
+// content require get server 
+// let http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+const os = require('os');
+console.log(os.platform());
+
